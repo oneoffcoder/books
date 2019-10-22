3fd014dc9bb37ce63fe52afd647448a5b267a2c1
Jee Vang, Ph.D <vangjee@gmail.com>
2019-10-22 17:58:56 -0400

    configure and add icons

--- spark-intro/source/conf.py
+++ spark-intro/source/conf.py
@@ -21,6 +21,10 @@ project = 'Spark, No Tears'
 copyright = '2019, One-Off Coder'
 author = 'One-Off Coder'
 
+# The short X.Y version
+version = ''
+# The full version, including alpha/beta/rc tags
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 
@@ -28,6 +32,7 @@ author = 'One-Off Coder'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxcontrib.bibtex'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -37,16 +42,17 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
+source_suffix = '.rst'
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_show_sourcelink = False
