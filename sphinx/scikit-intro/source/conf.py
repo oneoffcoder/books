9edc3b79a299c70fc7423e83b93469580ee75273
Jee Vang, Ph.D <vangjee@gmail.com>
2019-10-27 16:34:43 -0400

    change color

--- sphinx/scikit-intro/source/conf.py
+++ sphinx/scikit-intro/source/conf.py
@@ -65,7 +65,7 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    'style_nav_header_background': '#203731',
+    'style_nav_header_background': '#FF3C00',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
