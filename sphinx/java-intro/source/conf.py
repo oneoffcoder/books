57e29eaa544fe78a15207cefce50b9e6cb7a0e88
Jee Vang, Ph.D <vangjee@gmail.com>
2019-10-27 15:54:15 -0400

    add some customization

--- sphinx/java-intro/source/conf.py
+++ sphinx/java-intro/source/conf.py
@@ -52,3 +52,19 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_show_sourcelink = False
+
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': 'UA-150715122-1',  #  Provided by Google in your dashboard
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': '#A71930',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
