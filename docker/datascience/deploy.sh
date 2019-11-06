0dc52fb87e5900248dfb9c42e3e4625bde9c9948
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-06 15:50:30 -0500

    add more packages

--- docker/datascience/deploy.sh
+++ docker/datascience/deploy.sh
@@ -2,7 +2,7 @@
 
 ORGANIZATION=oneoffcoder
 REPOSITORY=book-datascience
-VERSION=0.0.1
+VERSION=0.0.2
 IMAGEID=$(docker images | awk -v repo="$REPOSITORY" -v tag="local" 'index($1, repo) && index($2, tag) {print $3}')
 
 echo ${IMAGEID}
