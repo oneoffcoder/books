0182c0af95edbe06f475aef28cd35f257424b328
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-05 20:04:25 -0500

    add run scripts

--- docker/python-intro/run.sh
+++ docker/python-intro/run.sh
@@ -2,7 +2,6 @@
 
 docker run -it \
     -p 8888:8888 \
-    -v $HOME/git/books/sphinx/pytorch-intro/source:/root/ipynb \
+    -v $HOME/git/books/docker/python-intro/ipynb:/root/ipynb \
     -e NOTEBOOK_PASSWORD=sha1:6676da7235c8:9c7d402c01e330b9368fa9e1637233748be11cc5 \
-    --gpus all \
-    book-pytorch-intro-gpu:local
+    book-python-intro:local
