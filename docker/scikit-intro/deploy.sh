87726307864d61d885ec99e6db7de5c228cf07d7
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-05 22:32:38 -0500

    update conda

--- docker/scikit-intro/deploy.sh
+++ docker/scikit-intro/deploy.sh
@@ -2,7 +2,7 @@
 
 ORGANIZATION=oneoffcoder
 REPOSITORY=book-scikit-intro
-VERSION=0.0.3
+VERSION=0.0.4
 IMAGEID=$(docker images | awk -v repo="$REPOSITORY" -v tag="local" 'index($1, repo) && index($2, tag) {print $3}')
 
 echo ${IMAGEID}
