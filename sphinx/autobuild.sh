24a13d838d6a4da2beb4547d5f674ab1b3b0a43e
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-06 20:42:16 -0500

    add project

--- sphinx/autobuild.sh
+++ sphinx/autobuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-projects=( "java-intro" "python-intro" "pytorch-intro" "scikit-intro" "spark-intro" )
+projects=( "java-intro" "python-intro" "pytorch-intro" "scikit-intro" "spark-intro" "datascience" )
 port=8000
 
 for project in "${projects[@]}"
