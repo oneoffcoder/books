caf2485fded759673cb1e28a747c290170b88bcb
Tor Vang <vang.tor@gmail.com>
2019-11-18 21:23:59 -0500

    modify

--- sphinx/excel/source/conf.py
+++ sphinx/excel/source/conf.py
@@ -22,6 +22,7 @@ copyright = '2019, One-Off Coder'
 author = 'One-Off Coder'
 
 # The full version, including alpha/beta/rc tags
+version = '0.0.1'
 release = '0.0.1'
 
 
@@ -48,8 +49,29 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False
+html_show_sphinx = False
+html_logo = '_static/images/logo-small.png'
+html_favicon = '_static/favicon.ico'
+html_css_files = ['css/override.css']
+html_last_updated_fmt = '%b %d, %Y, %X'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',  #  Provided by Google in your dashboard
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': '#D50A0A',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
