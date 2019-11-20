3f761d976221fd8f261b1d920f6768451f6ae24e
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-19 21:29:19 -0500

    change color; make executable

--- sphinx/makeblock/source/conf.py
+++ sphinx/makeblock/source/conf.py
@@ -69,7 +69,7 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    'style_nav_header_background': '#D50A0A',
+    'style_nav_header_background': '#A07A22',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
