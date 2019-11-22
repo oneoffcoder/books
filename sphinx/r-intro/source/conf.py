f2a6ef9fe073b172df83b79c24bd1c657f3788bc
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-21 21:10:38 -0500

    update color

--- sphinx/r-intro/source/conf.py
+++ sphinx/r-intro/source/conf.py
@@ -78,7 +78,7 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    'style_nav_header_background': '#D8A311',
+    'style_nav_header_background': '#001D3D',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
