95e4a80b0ee1b59c5e7b3065d0746f5418aed0fc
Jee Vang <super@Shengs-MBP.fios-router.home>
2019-11-22 10:10:16 -0500

    run the official image not local

--- docker/pytorch-intro-gpu/run.sh
+++ docker/pytorch-intro-gpu/run.sh
@@ -7,4 +7,4 @@ docker run -it \
     -v $HOME/git/books/sphinx/pytorch-intro/source/tensorboard:/root/tensorboard \
     -e NOTEBOOK_PASSWORD=sha1:6676da7235c8:9c7d402c01e330b9368fa9e1637233748be11cc5 \
     --gpus all \
-    book-pytorch-intro-gpu:local
+    oneoffcoder/book-pytorch-intro-gpu:latest
--- docker/scikit-intro/run.sh
+++ docker/scikit-intro/run.sh
@@ -4,4 +4,4 @@ docker run -it \
     -p 8888:8888 \
     -v $HOME/git/books/sphinx/scikit-intro/source:/root/ipynb \
     -e NOTEBOOK_PASSWORD=sha1:6676da7235c8:9c7d402c01e330b9368fa9e1637233748be11cc5 \
-    book-scikit-intro:local
+    oneoffcoder/book-scikit-intro:latest
