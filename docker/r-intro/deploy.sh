dc7e5ab8abff06cbc70f66f07f5f0262b40055f2
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-23 14:17:57 -0500

    add caret

--- docker/r-intro/deploy.sh
+++ docker/r-intro/deploy.sh
@@ -2,7 +2,7 @@
 
 ORGANIZATION=oneoffcoder
 REPOSITORY=book-r-intro
-VERSION=0.0.6
+VERSION=0.0.7
 IMAGEID=$(docker images | awk -v repo="$REPOSITORY" -v tag="local" 'index($1, repo) && index($2, tag) {print $3}')
 
 echo ${IMAGEID}
