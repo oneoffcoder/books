a979e202734767fb8709069bc06ab8d034f775a3
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-27 21:49:14 -0500

    add service + env

--- sphinx/docker/source/_static/code/compose/ng/ui-app/src/app/app.component.ts
+++ sphinx/docker/source/_static/code/compose/ng/ui-app/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from './service/student.service';
+import { Student } from './bo/student';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ui-app';
+export class AppComponent implements OnInit {
+  
+  constructor(private studentService: StudentService) { 
+    
+  }
+
+  ngOnInit(): void {
+    // this.studentService.readAll()
+    //   .subscribe(r => {
+    //     console.log(r)
+    //     const x = r.map(s => Student.toJson(s));
+    //     console.log(x);
+    //   }, e => console.error(e))
+
+    // this.studentService.read(1)
+    //   .subscribe(r => console.log(r), e => console.error(e));
+
+    // const student = Student.fromJson({ first_name: 'Zion', last_name: 'T', gender: 'M'});
+    // console.log(`student=${student}`)
+    // this.studentService.create(student)
+    //   .subscribe(r => console.log(r), e => console.error(e));
+
+    // const student = Student.fromJson({ id: 4, first_name: 'Zionx', last_name: 'T', gender: 'M'});
+    // this.studentService.update(student)
+    //   .subscribe(r => console.log(r), e => console.error(e));
+
+    // this.studentService.delete(7)
+    //   .subscribe(r => console.log(r), e => console.error(e));
+  }
 }
--- sphinx/docker/source/_static/code/compose/ng/ui-app/src/environments/environment.prod.ts
+++ sphinx/docker/source/_static/code/compose/ng/ui-app/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://rest:5000'
 };
