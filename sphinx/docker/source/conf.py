91b7916a71f65f5b4e8bf316083387348744d7d3
Jee Vang, Ph.D <vangjee@gmail.com>
2019-11-27 23:05:31 -0500

    ignore

--- sphinx/docker/source/conf.py
+++ sphinx/docker/source/conf.py
@@ -40,7 +40,9 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [
+    '**node_modules'
+]
 source_suffix = '.rst'
 
 # -- Options for HTML output -------------------------------------------------
