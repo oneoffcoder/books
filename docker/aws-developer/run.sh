c1dc5216df24a149b5fd9e1de2f07ffa6cf02def
vagrant <vagrant@oneoffcoder-1575232441981>
2019-12-01 17:37:10 -0500

    enable setting aws cli credentials

--- docker/aws-developer/run.sh
+++ docker/aws-developer/run.sh
@@ -4,4 +4,7 @@ docker run -it \
     -p 8888:8888 \
     -v $HOME/git/books/sphinx/aws-developer/source:/root/ipynb \
     -e NOTEBOOK_PASSWORD=sha1:6676da7235c8:9c7d402c01e330b9368fa9e1637233748be11cc5 \
-    oneoffcoder/book-aws-developer:latest
+    -e AWS_REGION="us-east-1x" \
+    -e AWS_ACCESS_KEY="NEED_TO_SET_KEYX" \
+    -e AWS_SECRET_KEY="NEED_TO_SET_SECRETX" \
+    book-aws-developer:local
