60a7103a129766cf72f9fc39e75bb8d0354f7d91
vagrant <vagrant@oneoffcoder-1575232441981>
2019-12-01 16:33:19 -0500

    add

---
+++ docker/aws-developer/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run -it \
+    -p 8888:8888 \
+    -v $HOME/git/books/sphinx/aws-developer/source:/root/ipynb \
+    -e NOTEBOOK_PASSWORD=sha1:6676da7235c8:9c7d402c01e330b9368fa9e1637233748be11cc5 \
+    oneoffcoder/book-aws-developer:latest
