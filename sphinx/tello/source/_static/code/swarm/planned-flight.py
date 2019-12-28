330673376ecc68b29d729592e1f049681c2b8969
Jee Vang, Ph.D <vangjee@gmail.com>
2019-12-28 13:30:02 -0500

    update

--- sphinx/tello/source/_static/code/swarm/planned-flight.py
+++ sphinx/tello/source/_static/code/swarm/planned-flight.py
@@ -8,10 +8,23 @@ import binascii
 from contextlib import suppress
 
 def create_execution_pools(num):
+    """
+    Creates execution pools.
+
+    :param num: Number of execution pools to create.
+    :return: List of Queues.
+    """
     return [Queue.Queue() for x in range(num)]
 
 
 def drone_handler(tello, queue):
+    """
+    Drone handler.
+
+    :param tello: Tello.
+    :param queue: Queue.
+    :return: None.
+    """
     while True:
         while queue.empty():
             pass
@@ -19,26 +32,50 @@ def drone_handler(tello, queue):
         tello.send_command(command)
 
 
-def all_queue_empty(execution_pools):
-    for queue in execution_pools:
+def all_queue_empty(pools):
+    """
+    Checks if all queues are empty.
+
+    :param pools: List of Queues.
+    :return: Boolean indicating if all queues are empty.
+    """
+    for queue in pools:
         if not queue.empty():
             return False
     return True
 
 
 def all_got_response(manager):
-    for tello_log in manager.get_log().values():
-        if not tello_log[-1].got_response():
+    """
+    Checks if all responses are received.
+
+    :param manager: TelloManager.
+    :return: A boolean indicating if all responses are received.
+    """
+    for log in self.manager.get_last_logs():
+        if not log.got_response():
             return False
     return True
 
 
 def create_dir(dpath):
+    """
+    Creates a directory if it does not exists.
+
+    :param dpath: Directory path.
+    :return: None.
+    """
     if not os.path.exists(dpath):
         with suppress(Exception):
             os.makedirs(dpath)
 
 def save_log(manager):
+    """
+    Saves the logs into a file in the ./log directory.
+
+    :param manager: TelloManager.
+    :return: None.
+    """
     dpath = './log'
     create_dir(dpath)
 
@@ -59,6 +96,15 @@ def save_log(manager):
 
 
 def check_timeout(start_time, end_time, timeout):
+    """
+    Checks if the duration between the end and start times
+    is larger than the specified timeout.
+
+    :param start_time: Start time.
+    :param end_time: End time.
+    :param timeout: Timeout threshold.
+    :return: A boolean indicating if the duration is larger than the specified timeout threshold.
+    """
     diff = end_time - start_time
     time.sleep(0.1)
     return diff > timeout
