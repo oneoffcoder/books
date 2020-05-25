33ba2287df2a2ad0edc707257b724a8e1563b217
Jee Vang, Ph.D <vangjee@gmail.com>
2020-05-25 15:55:25 -0400

    add

---
+++ docker/cpp/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run -it \
+    -p 8888:8888 \
+    -v $HOME/git/books/docker/java-intro/ipynb:/root/ipynb \
+    -e NOTEBOOK_PASSWORD=sha1:6676da7235c8:9c7d402c01e330b9368fa9e1637233748be11cc5 \
+    oneoffcoder/book-java-intro:latest
