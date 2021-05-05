7b81d6072dac36c94bf618e56f8d2331f582fa68
Jee Vang, Ph.D <vangjee@gmail.com>
2021-05-05 02:03:22 -0400

    add

---
+++ sphinx/csharp/IntroCode/ClassLibrary01/Class00Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary01
+{
+    public class Class00Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, world!");
+        }
+    }
+}
---
+++ sphinx/csharp/IntroCode/ClassLibrary01/Class01Print.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ClassLibrary01
+{
+    public class Class01Print
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("coding is fun!");
+            Console.WriteLine("abraham lincoln is the 16th president");
+        }
+    }
+}
