e0a2ccab98b5a5b93dd284e221bb788700a7ef6a
Jee Vang, Ph.D <vangjee@gmail.com>
2021-05-06 01:20:31 -0400

    add comments

--- sphinx/csharp/IntroCode/ClassLibrary01/C10StringMethods.cs
+++ sphinx/csharp/IntroCode/ClassLibrary01/C10StringMethods.cs
@@ -11,12 +11,14 @@ namespace ClassLibrary01
             var s2 = "hello, world!";
             var s3 = "hello, world";
             
+            // string comparison
             Console.WriteLine($"s1.equals(s2): {s1.Equals(s2)}");
             Console.WriteLine($"s1.equals(s3): {s1.Equals(s3)}");
             
             Console.WriteLine($"s1 == s2: {s1 == s2}");
             Console.WriteLine($"s1 == s3: {s1 == s3}");
 
+            // string methods
             Console.WriteLine(s1.StartsWith("hello"));
             Console.WriteLine(s1.EndsWith("world"));
             Console.WriteLine(s1.Replace("world", "earth"));
