36af216bde9ef8168415fe349452e0dd0a2e6a18
Jee Vang, Ph.D <vangjee@gmail.com>
2021-05-06 02:26:07 -0400

    add

---
+++ sphinx/csharp/IntroCode/ClassLibrary04/C00Input.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary04
+{
+    public class C00Input
+    {
+        static void Main(string[] args)
+        {
+            // getting input from the user
+            Console.WriteLine("name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"hello, {name}!");
+        }
+    }
+}
---
+++ sphinx/csharp/IntroCode/ClassLibrary04/C03Input.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary04
+{
+    class C03Input
+    {
+        static void Main(string[] args)
+        {
+            // program to add, subtract, multiply and divide 2 numbers
+            Console.WriteLine("num: ");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("num: ");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+        }
+    }
+}
