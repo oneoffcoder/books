72189c1230ce7f93363586de338bb0932fa4ceba
Jee Vang, Ph.D <vangjee@gmail.com>
2021-09-11 10:40:11 -0400

    fix script

--- docker/spark-intro/run.sh
+++ docker/spark-intro/run.sh
@@ -2,6 +2,5 @@
 
 docker run -it \
     -p 8888:8888 \
-    -v $HOME/git/books/sphinx/scikit-intro/source:/root/ipynb \
-    -e NOTEBOOK_PASSWORD=sha1:6676da7235c8:9c7d402c01e330b9368fa9e1637233748be11cc5 \
-    oneoffcoder/book-scikit-intro:latest
+    -v $HOME/git/books/sphinx/spark-intro/source:/root/ipynb \
+    oneoffcoder/book-spark-intro:latest
