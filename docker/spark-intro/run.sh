0153f364b84c1563d4e9dbccb120843eb22d006a
Jee Vang, Ph.D <vangjee@gmail.com>
2021-09-12 01:32:28 -0400

    open ports

--- docker/spark-intro/run.sh
+++ docker/spark-intro/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 docker run -it \
+    -p 9870:9870 \
+    -p 8088:8088 \
+    -p 8080:8080 \
+    -p 18080:18080 \
     -p 8888:8888 \
     -v $HOME/git/books/sphinx/spark-intro/source:/root/ipynb \
     oneoffcoder/book-spark-intro:latest
