1b715e4331c5cbc64910094a065499c02a2597ee
Jee Vang, Ph.D <vangjee@gmail.com>
2021-12-30 18:18:23 -0500

    fix connection string

--- sphinx/docker/source/_static/code/ecs/rest/app/api/database.py
+++ sphinx/docker/source/_static/code/ecs/rest/app/api/database.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://oneoffcoder:isthebest@db/ecsdb"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://oneoffcoder:isthebest@db/ecsdb"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
