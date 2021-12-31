4462fc21cac513a15d54a1eb7ba947b5f62157f4
Jee Vang, Ph.D <vangjee@gmail.com>
2021-12-30 20:35:07 -0500

    externalize database url

--- sphinx/docker/source/_static/code/ecs/rest/app/api/database.py
+++ sphinx/docker/source/_static/code/ecs/rest/app/api/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://oneoffcoder:isthebest@db/ecsdb"
+SQLALCHEMY_DATABASE_URL = f'{os.environ.get("DATABASE_URL")}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
