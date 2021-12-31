202922e66ce5ec0d28d1cb3e7485babf5f089f47
Jee Vang, Ph.D <vangjee@gmail.com>
2021-12-30 21:03:33 -0500

    only create if not exists

--- sphinx/docker/source/_static/code/ecs/db/docker-entrypoint-initdb.d/setup.sql
+++ sphinx/docker/source/_static/code/ecs/db/docker-entrypoint-initdb.d/setup.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE ecsdb;
+CREATE DATABASE IF NOT EXISTS ecsdb;
 
-CREATE USER 'oneoffcoder'@'localhost' IDENTIFIED BY 'isthebest';
-CREATE USER 'oneoffcoder'@'%' IDENTIFIED BY 'isthebest';
+CREATE USER IF NOT EXISTS 'oneoffcoder'@'localhost' IDENTIFIED BY 'isthebest';
+CREATE USER IF NOT EXISTS 'oneoffcoder'@'%' IDENTIFIED BY 'isthebest';
 
 GRANT ALL PRIVILEGES ON ecsdb.* TO 'oneoffcoder'@'localhost';
 GRANT ALL PRIVILEGES ON ecsdb.* TO 'oneoffcoder'@'%';
 
 USE ecsdb;
 
-CREATE TABLE person (
+CREATE TABLE IF NOT EXISTS person (
     id smallint unsigned not null auto_increment, 
     first_name varchar(50) not null, 
     last_name varchar(50) not null, 
@@ -17,9 +17,14 @@ CREATE TABLE person (
     constraint pk_example primary key (id) 
 );
 
-INSERT INTO person (first_name, last_name, gender, age) VALUES 
-('John', 'Doe', 'M', 32),
-('Jane', 'Smith', 'F', 31),
-('Joe', 'Smith', 'M', 28),
-('Joyce', 'Doe', 'F', 29)
+INSERT INTO person (id, first_name, last_name, gender, age) VALUES 
+(1, 'John', 'Doe', 'M', 32),
+(2, 'Jane', 'Smith', 'F', 31),
+(3, 'Joe', 'Smith', 'M', 28),
+(4, 'Joyce', 'Doe', 'F', 29)
+ON DUPLICATE KEY UPDATE 
+    first_name=VALUES(first_name),
+    last_name=VALUES(last_name),
+    gender=VALUES(gender),
+    age=VALUES(age)
 ;
