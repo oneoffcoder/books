f52b3f33d0ff0e5dae6da1e8c5291ed066c10b8e
Jee Vang, Ph.D <vangjee@gmail.com>
2022-01-04 02:26:36 -0500

    update

--- sphinx/docker/source/_static/code/ecs/www/src/app/api/service.ts
+++ sphinx/docker/source/_static/code/ecs/www/src/app/api/service.ts
@@ -13,8 +13,8 @@ export class ApiService {
 
   constructor(private http: HttpClient) {
     // FIXME: remove later
-    const baseUrl = environment.apiService.urls.base.replace(':4200', '');
-    // const baseUrl = environment.apiService.urls.base;
+    // const baseUrl = environment.apiService.urls.base.replace(':4200', '');
+    const baseUrl = environment.apiService.urls.base;
     this.healthUrl = `${baseUrl}/${environment.apiService.urls.health}`;
     this.personUrl = `${baseUrl}/${environment.apiService.urls.person}`;
   }
